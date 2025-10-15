532511b6e5564b450738284825fb9935315b3eae
Mathew Kuthur James <mathewkj2048@gmail.com>
2025-10-15 16:25:20 -0400

    top-level Main class

---
+++ app/src/main/java/ca/uwaterloo/watform/Main.java
@@ -0,0 +1,12 @@
+package ca.uwaterloo.watform;
+
+
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		
+		System.out.println("Hello from the top-level class");
+
+		ca.uwaterloo.watform.parser.Main.main(args);
+	}
+}
