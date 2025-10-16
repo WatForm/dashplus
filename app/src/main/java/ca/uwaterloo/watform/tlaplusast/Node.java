14b1643b7766ceb2753b2aba96106f7171d28dce
Mathew Kuthur James <mathewkj2048@gmail.com>
2025-10-15 21:45:53 -0400

    moved strings to ast - tla+

--- app/src/main/java/ca/uwaterloo/watform/tlaplusast/Node.java
+++
@@ -1,7 +0,0 @@
-
-package ca.uwaterloo.watform.tlaplusast;
-
-abstract public class Node
-{
-	// todo: see if an interface is better suited for the ToString enforcement
-}
--- app/src/main/java/ca/uwaterloo/watform/tlaplusast/TLAPlusStrings.java
+++ app/src/main/java/ca/uwaterloo/watform/tlaplusast/TLAPlusStrings.java
@@ -1,4 +1,4 @@
-package ca.uwaterloo.watform.utils;
+package ca.uwaterloo.watform.tlaplusast;
 
 public final class TLAPlusStrings 
 {
