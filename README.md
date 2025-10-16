b275606ec4abb5ea32f53541c0fc7f299d64e9c0
Mathew Kuthur James <mathewkj2048@gmail.com>
2025-10-15 20:45:58 -0400

    categorizing TLAPlus strings

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Building from source:
 
-Building from source requires the java version to be >= 25. The java version can be verified by running `java --version`. Java 25 can be installed from [https://jdk.java.net/25/](https://jdk.java.net/25/)
+Building from source requires the java version to be >= 17. The java version can be verified by running `java --version`. 
 
 To build the .jar file, run the following:
 
@@ -16,7 +16,7 @@ The generated .jar file is located at `dashplus/app/build/libs/app.jar`
 
 The build process uses gradle version 9.1.0. However, installing this globally is unnecessary, since it is downloaded as part of the build process.
 
-The build process is the same for Linux, MacOS and Windows (powershell). On cmd, `./gradlew build` is replaced by `gradlew.bat`
+The build process is the same for Linux, MacOS and Windows powershell. On Windows cmd, `./gradlew build` is replaced by `gradlew.bat`
 
 
 ## Instructions for Contributors:
--- app/src/main/java/ca/uwaterloo/watform/utils/TLAPlusStrings.java
+++ app/src/main/java/ca/uwaterloo/watform/utils/TLAPlusStrings.java
@@ -2,32 +2,78 @@ package ca.uwaterloo.watform.utils;
 
 public final class TLAPlusStrings 
 {
+	// top-level keywords
+	public static final String MODULE = "MODULE";
+	public static final String EXTENDS = "EXTENDS";
+	public static final String VARIABLES = "VARIABLES";
+	public static final String CONSTANTS = "CONSTANTS";
+
+	// top-level structure
+	public static final String HEAD_DELIMITER = "----";
+	public static final String BODY_DELIMITER = "====";
+
+	// comments
+	public static final String MULTI_COMMENT_START = "(*";
+	public static final String MULTI_COMMENT_END = "*)";
+	public static final String SINGLE_COMMENT = "\\*";
+
+	// strings
+	public static final String STRING_START = "\"";
+	public static final String STRING_END = STRING_START;
+
+	// sets
+	public static final String SET_START = "{";
+	public static final String SET_END = "}";
+	public static final String SET_IN = "\\in";
+	public static final String SET_NOT_IN = "\\notin";
+	public static final String SET_INTERSECTION = "\\intersect";
+	public static final String SET_UNION = "\\union";
+	public static final String SET_SUBSET_EQ = "\\subseteq";
+	public static final String SET_DIFFERENCE = "\\";
+	public static final String SET_PRODUCT = "\\X";
+
+	// functions
+	public static final String EXISTS = "\\E";
+	public static final String FORALL = "\\A";
+	public static final String MAP = "|->";
+	
+	// booleans
 	public static final String BOOLEAN = "BOOLEAN";
 	public static final String TRUE = "TRUE";
 	public static final String FALSE = "FALSE";
-
 	public static final String AND = "/\\";
 	public static final String OR = "\\/";
 	public static final String NOT = "~";
 	public static final String IMPLICATION = "=>";
 	public static final String EQUIVALENCE = "<=>";
 
+	// integers
+	public static final String INT_SET = "";
+	public static final String RANGE = "";
+	public static final String PLUS = "";
+	public static final String MINUS = "";
+	public static final String TIMES = "";
+	public static final String LESSER_THAN = "";
+	public static final String LESSER_THAN_EQUALS = "";
+	public static final String GREATER_THAN = "";
+	public static final String GREATER_THAN_EQUALS = "";
 	public static final String EQUALS = "=";
 	public static final String NOT_EQUALS = "/=";
 
-	public static final String SET_IN = "\\in";
-	public static final String SET_NOT_IN = "\\notin";
-	public static final String SET_INTERSECTION = "\\intersect";
-	public static final String SET_UNION = "\\union";
-	public static final String SET_SUBSETEQ = "\\subseteq";
-	public static final String SET_DIFFERENCE = "\\";
+	
 
 	public static final String SEQUENCE_OPEN = "<<";
 	public static final String SEQUENCE_CLOSE = ">>";
 	public static final String TUPLE_OPEN = SEQUENCE_OPEN;
 	public static final String TUPLE_CLOSE = SEQUENCE_CLOSE;
 
+	public static final String COMMA = ",";
+	public static final String COLON = ":";
+	public static final String SQUARE_BRACKET_OPEN = "[";
+	public static final String SQUARE_BRACKET_CLOSE = "]";
+
 	public static final String PRIME = "'";
+	public static final String UNCHANGED = "UNCHANGED";
 
 	public static final String LET = "LET";
 	public static final String IN = "IN";
@@ -35,6 +81,21 @@ public final class TLAPlusStrings
 	public static final String THEN = "THEN";
 	public static final String ELSE = "ELSE";
 
+	// inbuilt functions
+	public static final String HEAD = "Head";
+	public static final String TAIL = "Tail";
+	public static final String LEN = "Len";
+	public static final String APPEND = "Append";
+	public static final String CONCATENATE = "\\o";
+	public static final String CARDINALITY = "Cardinality";
+	
+
+	// Standard Modules
+	public static final String BAGS = "Bags";
+	public static final String FINITE_SETS = "FiniteSets";
+	public static final String INTEGERS = "Integers";
+	public static final String SEQUENCES = "Sequences";
+	public static final String NATURALS = "Naturals";
 
 
 	// public static final String = "";
