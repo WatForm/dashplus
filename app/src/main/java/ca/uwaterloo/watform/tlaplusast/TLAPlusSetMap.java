9ffa809b3112db33db6ac34918d7a3392fe2cb7f
Mathew Kuthur James <mathewkj2048@gmail.com>
2025-10-20 07:11:10 -0400

    set map

---
+++ app/src/main/java/ca/uwaterloo/watform/tlaplusast/TLAPlusSetMap.java
@@ -0,0 +1,30 @@
+package ca.uwaterloo.watform.tlaplusast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TLAPlusSetMap extends TLAPlusExp
+{
+	public TLAPlusSetMap(TLAPlusASTNode exp, TLAPlusVariable v, TLAPlusASTNode set)
+	{
+		this.children = new ArrayList<>();
+		this.children.add(exp);
+		this.children.add(v);
+		this.children.add(set);
+	}
+
+	@Override
+	public List<String> toStringList() 
+	{
+		List<String> result = new ArrayList<>();
+		result.add(TLAPlusStrings.SET_START);
+		result.addAll(this.children.get(0).toStringList());
+		result.add(TLAPlusStrings.COLON);
+		result.addAll(this.children.get(1).toStringList());
+		result.add(TLAPlusStrings.SET_IN);
+		result.addAll(this.children.get(2).toStringList());
+		result.add(TLAPlusStrings.SET_END);
+		return result;
+	}	
+	
+}
