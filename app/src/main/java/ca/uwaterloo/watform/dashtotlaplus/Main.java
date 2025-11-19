7c03124052798cf25687ee49a26976e89af9b692
Mathew Kuthur James <mathewkj2048@gmail.com>
2025-11-19 12:34:58 -0500

    reading files

--- app/src/main/java/ca/uwaterloo/watform/dashtotlaplus/Main.java
+++ app/src/main/java/ca/uwaterloo/watform/dashtotlaplus/Main.java
@@ -2,11 +2,27 @@ package ca.uwaterloo.watform.dashtotlaplus;
 
 import ca.uwaterloo.watform.tlaplusast.*;
 import ca.uwaterloo.watform.tlaplusmodel.*;
+import java.io.IOException;
+import java.nio.file.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
         System.out.println("Hello from the dashtotlaplus class");
+
+        if (args.length != 2) {
+            System.out.println("Path to input file.dsh and output file.tla are needed");
+            return;
+        }
+
+        try {
+            Path inPath = Paths.get(args[0]);
+            Path outPath = Paths.get(args[1]);
+            String input = Files.readString(inPath);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         TLAPlusModule module = new TLAPlusModule("test");
 
