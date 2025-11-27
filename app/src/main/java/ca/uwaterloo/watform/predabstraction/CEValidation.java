7a338a01ad4e5e468714f17d818fb0712ff69d88
Qiyue Chen <jackchen20048@gmail.com>
2025-11-27 14:20:55 -0500

    CEValidation: separate preds

--- app/src/main/java/ca/uwaterloo/watform/predabstraction/CEValidation.java
+++ app/src/main/java/ca/uwaterloo/watform/predabstraction/CEValidation.java
@@ -1,11 +1,10 @@
 package ca.uwaterloo.watform.predabstraction;
 
-import ca.uwaterloo.watform.alloyast.expr.AlloyExpr;
 import ca.uwaterloo.watform.alloyast.expr.binary.AlloyEqualsExpr;
 import ca.uwaterloo.watform.alloyast.expr.misc.AlloyBlock;
 import ca.uwaterloo.watform.alloyast.expr.misc.AlloyBracketExpr;
 import ca.uwaterloo.watform.alloyast.expr.var.AlloyQnameExpr;
-import ca.uwaterloo.watform.alloyast.paragraph.AlloyFactPara;
+import ca.uwaterloo.watform.alloyast.paragraph.AlloyPredPara;
 import ca.uwaterloo.watform.alloyast.paragraph.sig.AlloySigPara;
 import ca.uwaterloo.watform.alloyinterface.Solution;
 import ca.uwaterloo.watform.alloymodel.AlloyModel;
@@ -19,6 +18,7 @@ import java.util.Map;
 import java.util.Set;
 
 public final class CEValidation {
+    public static final String snShot = "snShot";
 
     public static void initAndNext(AlloyModel alloyModel, Solution solution) {
         // Get the next relation on the Snapshots
@@ -37,22 +37,23 @@ public final class CEValidation {
         // Rename snapshots; DshSnapshot$0 may not be the first snapshot and
         // it's confusing
         Map<String, String> origToRenamedSnap = new HashMap<>();
-        origToRenamedSnap.put(firstSnap, DashStrings.snapshotName + String.valueOf(0));
+        origToRenamedSnap.put(firstSnap, CEValidation.snShot + String.valueOf(0));
         List<String> renamedSnaps = new ArrayList<>(); // ordered by the next relation
-        renamedSnaps.add(DashStrings.snapshotName + String.valueOf(0));
+        renamedSnaps.add(CEValidation.snShot + String.valueOf(0));
 
         int count = 1;
         String currSnap = firstSnap;
 
         while (nextRelSnapMap.containsKey(currSnap)) {
             String nextSnap = nextRelSnapMap.get(currSnap);
-            String renamedNextSnap = DashStrings.snapshotName + String.valueOf(count);
+            String renamedNextSnap = CEValidation.snShot + String.valueOf(count);
             origToRenamedSnap.put(nextSnap, renamedNextSnap);
             renamedSnaps.add(renamedNextSnap);
             count++;
             currSnap = nextSnap;
         }
 
+        // ---------------------------------------
         // add sigs
         for (String renamedSnap : renamedSnaps) {
             alloyModel.addPara(
@@ -64,27 +65,32 @@ public final class CEValidation {
                             new AlloyBlock()));
         }
 
-        // add Fact
-        List<AlloyExpr> alloyExprs = new ArrayList<>();
-        // add init
-        alloyExprs.add(
-                new AlloyBracketExpr(
-                        new AlloyQnameExpr(DashStrings.initFactName),
-                        List.of(new AlloyQnameExpr(renamedSnaps.getFirst()))));
+        alloyModel.addPara(
+                new AlloyPredPara(
+                        renamedSnaps.getFirst() + "Pred",
+                        new AlloyBlock(
+                                List.of(
+                                        new AlloyBracketExpr(
+                                                new AlloyQnameExpr(DashStrings.initFactName),
+                                                List.of(
+                                                        new AlloyQnameExpr(
+                                                                renamedSnaps.getFirst())))))));
 
-        // add next
-        for (int i = 0; i < renamedSnaps.size() - 1; i++) {
-            alloyExprs.add(
-                    new AlloyEqualsExpr(
-                            new AlloyBracketExpr(
-                                    new AlloyQnameExpr(
-                                            DashStrings.snapshotName
-                                                    + DashStrings.SLASH
-                                                    + DashStrings.tracesNextName),
-                                    List.of(new AlloyQnameExpr(renamedSnaps.get(i)))),
-                            new AlloyQnameExpr(renamedSnaps.get(i + 1))));
+        for (int i = 1; i < renamedSnaps.size(); i++) {
+            alloyModel.addPara(
+                    new AlloyPredPara(
+                            renamedSnaps.get(i) + "Pred",
+                            new AlloyBlock(
+                                    new AlloyEqualsExpr(
+                                            new AlloyBracketExpr(
+                                                    new AlloyQnameExpr(
+                                                            DashStrings.snapshotName
+                                                                    + DashStrings.SLASH
+                                                                    + DashStrings.tracesNextName),
+                                                    List.of(
+                                                            new AlloyQnameExpr(
+                                                                    renamedSnaps.get(i - 1)))),
+                                            new AlloyQnameExpr(renamedSnaps.get(i))))));
         }
-
-        alloyModel.addPara(new AlloyFactPara(new AlloyBlock(alloyExprs)));
     }
 }
