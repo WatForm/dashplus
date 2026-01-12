4c8d9333484b80e4fd0cfe9363e907059a2b6ad3
nancyday <nday@uwaterloo.ca>
2026-01-12 16:13:18 -0500

    beginning skeleton for dash to Alloy translation

---
+++ app/src/main/java/ca/uwaterloo/watform/dashtoalloy/DashToAlloy.java
@@ -0,0 +1,21 @@
+
+// usage: AlloyModel am = new DashToAlloy(dm).translate()
+
+package ca.uwaterloo.watform.dashtoalloy;
+
+import ca.uwaterloo.watform.dashmodel.DashModel;
+import ca.uwaterloo.watform.alloymodel.AlloyModel;
+
+public class DashToAlloy extends SpaceSignatures {
+
+	public DashToAlloy(DashModel dm) {
+		super(dm);
+	}
+
+	public AlloyModel translate() {
+		this.am = new AlloyModel();
+		// TODO: copy all Alloy stuff from dm into am
+		this.addSpaceSignatures();
+		return this.am;
+	}
+}
---
+++ app/src/main/java/ca/uwaterloo/watform/dashtoalloy/SpaceSignatures.java
@@ -0,0 +1,20 @@
+package ca.uwaterloo.watform.dashtoalloy;
+
+import ca.uwaterloo.watform.dashmodel.DashModel;
+import ca.uwaterloo.watform.alloymodel.AlloyModel;
+
+public class SpaceSignatures {
+
+	DashModel dm; // input
+	AlloyModel am = new AlloyModel(); //output
+
+	protected SpaceSignatures(DashModel dm) {
+		this.dm = dm;
+	}
+
+	protected void addSpaceSignatures() {
+
+	}
+
+}
+
