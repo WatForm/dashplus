6bc7625b888545bba5fb35e2cab4d5287baed30a
Qiyue Chen <jackchen20048@gmail.com>
2026-01-20 21:07:04 -0500

    new cli inputs

--- app/src/main/java/ca/uwaterloo/watform/cli/Main.java
+++ app/src/main/java/ca/uwaterloo/watform/cli/Main.java
@@ -26,38 +26,89 @@ import picocli.CommandLine.Parameters;
             "@|cyan /_____/\\__,_/____/_/_/_/   /_/\\__,_/___/____/    |@",
             ""
         },
-        description = {
-            "Parse Dash/Alloy model files and execute command.",
-        },
         footer = {
             "",
-            "@|bold,underline OTHER TOOLS IN THIS JAR:|@",
-            "  This JAR contains the full Dash+ suite. You can invoke other " + "tools",
-            "  by specifying their class name:",
+            "@|bold,underline USAGE MODES|@",
             "",
-            "  @|yellow,bold [TLA]|@ ",
-            "    java -cp watform-dashplus.jar " + "ca.uwaterloo.watform.dashtotla.Main <args>",
+            // 1) Alloy Files
+            "  @|bold 1) dashplus f.als < -cmd | -cmd n > < -v > < -d > |@",
+            "     (execute cmd on alloy file)",
+            "     @|italic DEFAULT:|@ dashplus f.als means dashplus f.als -cmd (execute all cmds)",
             "",
-            "  @|yellow,bold [Predicate Abstraction]|@   ",
-            "    java -cp watform-dashplus.jar "
-                    + "ca.uwaterloo.watform.predabstraction.Main <args>",
+            // 2) Dash -> Alloy
+            "  @|bold 2) dashplus f.dsh -alloy < traces | tcmc | electrum >|@",
+            "              @|bold < -cmd | -cmd n | -write | -noCmd > < -s > < -v > < -d >|@",
+            "     (parse/translate to alloy/execute cmd(s) or write .als file in same dir)",
+            "     @|italic DEFAULT:|@ dashplus f.dsh means dashplus f.dsh -alloy traces -write",
             "",
+            // 3) Dash -> TLA
+            "  @|bold 3) dashplus f.dsh -tla < -s > < -v > < -d > < -p >|@",
+            "     (parse/translate to tla/outputs .tla file in same dir as input file)",
+            "",
+            // 4) Predicate Abstraction
+            "  @|bold 4) dashplus f.dsh -predAbs < -cmd | -cmd n > < -s > < -v > < -d >|@",
+            "     (parse/pred abstraction)",
+            "",
+            // 5) XML Instance Check
+            "  @|bold 5) dashplus f.dsh -xml instance.xml < -s > < -v > < -d >|@",
+            "     (parse/translate to Alloy/check if instance is instance of translated Alloy)",
+            "",
+            // General Flags
+            "@|bold,underline GENERAL FLAGS|@",
+            "  @|bold -s|@   for single environmental input",
+            "  @|bold -v|@   for verbose output - this adds in comments",
+            "  @|bold -d|@   debug mode, where the translator writes things during translation to stdout",
+            ""
         },
 
         // Optional: Customize section headings
         optionListHeading = "%n@|bold Options:|@%n",
         parameterListHeading = "%n@|bold Parameters:|@%n")
 public class Main implements Callable<Integer> {
-    @Option(
-            names = "-cmd",
-            arity = "0..1", // Makes it optional (0 or 1 occurrence)
-            defaultValue = "0",
-            paramLabel = "<cmdIdx>",
-            description = "Index of the command to execute (Default: ${DEFAULT-VALUE}).")
-    private int commandIndex;
+    public enum AlloyMode {
+        traces,
+        tcmc,
+        electrum
+    }
 
     @Parameters(index = "0", arity = "1..*", description = "Paths to Alloy/Dash files")
     private List<Path> inputPaths;
+
+    @Option(
+            names = "-alloy",
+            description = "Translation mode: ${COMPLETION-CANDIDATES}.",
+            defaultValue = "traces")
+    private AlloyMode alloyMode;
+
+    // 0 indexed
+    //
+    @Option(
+            names = "-cmd",
+            arity = "0..1", // Makes it optional (0 or 1 occurrence)
+            defaultValue = "-1",
+            paramLabel = "<cmdIdx>",
+            description = "Index of the command to execute (Default: execute all).")
+    private int commandIndex;
+
+    @Option(
+            names = {"-noCmd"},
+            description = "Check satisfiability without commands")
+    private boolean noCmd = false;
+
+    @Option(
+            names = {"-write"},
+            description = "Write translated Alloy into file")
+    private boolean write = false;
+
+    @Option(
+            names = {"-s", "--single"},
+            description = "Single environmental input")
+    private boolean single = false;
+
+    @Option(
+            names = {"-v", "--verbose"},
+            description = "Verbose output and see comments")
+    private boolean verbose = false;
 
     @Option(
             names = {"-d", "--debug"},
@@ -77,7 +128,8 @@ public class Main implements Callable<Integer> {
                 //
                 // tmp debugging end
                 // Solution solution =
-                //        AlloyInterface.executeCommand(parseToModel(path), this.commandIndex);
+                // AlloyInterface.executeCommand(parseToModel(path),
+                // this.commandIndex);
                 // System.out.println(solution.toString());
                 Reporter.INSTANCE.print();
                 // System.out.println("translation to Alloy ----");
