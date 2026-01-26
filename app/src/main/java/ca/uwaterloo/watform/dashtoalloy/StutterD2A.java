497632d73bee4104f2103abaa82000509c7011d4
nancyday <nday@uwaterloo.ca>
2026-01-26 13:41:07 -0500

    combined stutter methods

--- app/src/main/java/ca/uwaterloo/watform/dashtoalloy/StutterD2A.java
+++ app/src/main/java/ca/uwaterloo/watform/dashtoalloy/StutterD2A.java
@@ -79,4 +79,42 @@ public class StutterD2A extends InitsD2A {
                     AlloyVar(DashFQN.translateFQN(n))), 
                     this.dsl.curJoinExpr(AlloyVar(DashFQN.translateFQN(n))));
     }
+
+    /* optional for tcmc only
+
+    pred no_stutter {
+        all s:DshSnapshot |
+        s = tcmc/ks_s0 or 
+        NO_TRANS not in s.dsh_taken0 or
+        all p:Identifiers | NO_TRANS not in s.(p.dsh_taken0)
+        ... for all levels
+    */
+    public void addStrongNoStutter() {
+
+        assert(this.isTcmc);
+        AlloyExpr snapShotFirst = AlloyVar(D2AStrings.tcmcInitialStateName);
+        List<AlloyExpr> body = this.dsl.emptyExprList();
+        List<AlloyDecl> decls = this.dsl.emptyDecls();
+ 
+        List<AlloyExpr> bigOr = this.dsl.emptyExprList();
+        for (int i = 0; i <= this.dm.maxDepthParams(); i++) {
+            // don't need to make this stronger than an Or
+            // b/c other parts of semantics will make sure only
+            // one transTaken is true
+            bigOr.add(
+                AlloyNot(
+                    AlloyEqual(
+                        this.dsl.curTransTaken(i), 
+                        this.dsl.noneArrow(i))));
+        }
+        bigOr.add(AlloyEqual(
+                        this.dsl.curVar(), 
+                        snapShotFirst));
+        
+        decls.add(this.dsl.curDecl());
+        body.add(AlloyAllVars(decls,AlloyOrList(bigOr)));
+
+        this.addPred(D2AStrings.strongNoStutterName, this.dsl.emptyDecls(), body);
+
+    }
 }
