a3b4f1f79d15835c68753e6f6fd7021355d38a2c
Mathew Kuthur James <mathewkj2048@gmail.com>
2026-02-09 01:10:16 -0500

    set augementation for sigs

--- app/src/main/java/ca/uwaterloo/watform/alloytotla/AlloyToTlaStrings.java
+++ app/src/main/java/ca/uwaterloo/watform/alloytotla/AlloyToTlaStrings.java
@@ -7,4 +7,7 @@ public class AlloyToTlaStrings {
     // common formulae without arguments
     public static final String INIT = SPECIAL + "Init";
     public static final String NEXT = SPECIAL + "Next";
+
+    //
+    public static final String SIG_SET_SUFFIX = SPECIAL + "set";
 }
--- app/src/main/java/ca/uwaterloo/watform/alloytotla/SigVarConf.java
+++ app/src/main/java/ca/uwaterloo/watform/alloytotla/SigVarConf.java
@@ -6,4 +6,9 @@ import ca.uwaterloo.watform.tlamodel.TlaModel;
 public class SigVarConf {
 
     public static void translate(AlloyModel alloyModel, TlaModel tlaModel) {}
+
+    public static void makeSigSet(String sigName, AlloyModel alloyModel, TlaModel tlaModel) {
+        // S_set = {"S0","S1","S2"...}
+
+    }
 }
