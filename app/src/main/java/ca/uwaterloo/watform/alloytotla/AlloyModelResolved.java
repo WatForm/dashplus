596cd695b7abf1df39d54ffbda99aec464aaa154
Mathew Kuthur James <mathewkj2048@gmail.com>
2026-02-18 01:49:47 -0500

    added code for in-parents, single pass

--- app/src/main/java/ca/uwaterloo/watform/alloytotla/AlloyModelResolved.java
+++ app/src/main/java/ca/uwaterloo/watform/alloytotla/AlloyModelResolved.java
@@ -8,6 +8,7 @@ import ca.uwaterloo.watform.alloyast.paragraph.sig.AlloySigPara;
 import ca.uwaterloo.watform.alloyast.paragraph.sig.AlloySigPara.Qual;
 import ca.uwaterloo.watform.alloymodel.AlloyModel;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class AlloyModelResolved extends AlloyModel {
 
@@ -16,22 +17,32 @@ public class AlloyModelResolved extends AlloyModel {
         resolve();
     }
 
-    private static record SignatureRecord(
-            AlloySigPara para,
-            List<String> inParents,
-            Optional<String> extendsParent,
-            List<String> children,
-            List<String> topLevelParents,
-            List<String> descs,
-            List<String> ances) {}
+    private static class SignatureRecord {
+        AlloySigPara para;
+        List<String> inParents;
+        Optional<String> extendsParent;
+        List<String> children;
+        List<String> topLevelParents;
+        List<String> descs;
+        List<String> ances;
 
-    private static record FieldRecord(String sigParent, AlloyDecl decl) {}
+        SignatureRecord(AlloySigPara p) {
+            this.para = p;
+        }
+    }
+
+    private static class FieldRecord {
+        String sigParent;
+        AlloyDecl decl;
+    }
 
     private HashMap<String, SignatureRecord> sigTable;
     private HashMap<String, FieldRecord> fieldTable;
 
     private void resolve() {
         populateNames();
+        populateExtendsParent();
+        populateInParents();
     }
 
     private void populateNames() {
@@ -39,24 +50,62 @@ public class AlloyModelResolved extends AlloyModel {
                 .forEach(
                         sp -> {
                             String name = sp.qnames.get(0).toString();
-                            SignatureRecord r =
-                                    new SignatureRecord(sp, null, null, null, null, null, null);
+                            SignatureRecord r = new SignatureRecord(sp);
                             this.sigTable.put(name, r);
                         });
     }
 
-    private void populateInParents() {}
+    private void populateExtendsParent() {
+        this.sigTable
+                .keySet()
+                .forEach(
+                        sn -> {
+                            AtomicReference<String> answer = new AtomicReference<>(null);
+                            sigTable.get(sn)
+                                    .para
+                                    .rel
+                                    .ifPresent(
+                                            x -> {
+                                                if (x instanceof AlloySigPara.Extends)
+                                                    answer.set(
+                                                            ((AlloySigPara.Extends) x)
+                                                                    .sigRef.toString());
+                                            });
+                            sigTable.get(sn).extendsParent = Optional.ofNullable(answer.get());
+                        });
+    }
 
-    public List<String> getAllSigNames(String signame) {
+    private void populateInParents() {
+
+        this.sigTable
+                .keySet()
+                .forEach(
+                        sn -> {
+                            sigTable.get(sn)
+                                    .para
+                                    .rel
+                                    .ifPresent(
+                                            x -> {
+                                                if (x instanceof AlloySigPara.In)
+                                                    sigTable.get(sn).inParents =
+                                                            mapBy(
+                                                                    ((AlloySigPara.In) x).sigRefs,
+                                                                    s -> s.toString());
+                                                else sigTable.get(sn).inParents = new ArrayList<>();
+                                            });
+                        });
+    }
+
+    public List<String> getAllSigNames() {
         return new ArrayList<>(this.sigTable.keySet());
     }
 
-    public List<String> getTopLevelSigNames(String signame) {
-        return filterBy(getAllSigNames(signame), s -> this.isTopLevelSig(s));
+    public List<String> getTopLevelSigNames() {
+        return filterBy(getAllSigNames(), s -> this.isTopLevelSig(s));
     }
 
-    public List<String> getDescendedSigNames(String signame) {
-        return filterBy(getAllSigNames(signame), s -> !this.isTopLevelSig(s));
+    public List<String> getDescendedSigNames() {
+        return filterBy(getAllSigNames(), s -> !this.isTopLevelSig(s));
     }
 
     public boolean isTopLevelSig(String signame) {
