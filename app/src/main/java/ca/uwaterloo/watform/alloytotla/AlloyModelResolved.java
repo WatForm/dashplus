31c26f42a90fedd690b306dfe187a2b4fc63d6ac
Mathew Kuthur James <mathewkj2048@gmail.com>
2026-02-18 00:55:45 -0500

    accessors

--- app/src/main/java/ca/uwaterloo/watform/alloytotla/AlloyModelResolved.java
+++ app/src/main/java/ca/uwaterloo/watform/alloytotla/AlloyModelResolved.java
@@ -1,60 +1,69 @@
 package ca.uwaterloo.watform.alloytotla;
 
-import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
+import static ca.uwaterloo.watform.utils.GeneralUtil.*;
 
 import ca.uwaterloo.watform.alloyast.AlloyFile;
 import ca.uwaterloo.watform.alloyast.expr.misc.AlloyDecl;
 import ca.uwaterloo.watform.alloyast.paragraph.sig.AlloySigPara;
 import ca.uwaterloo.watform.alloyast.paragraph.sig.AlloySigPara.Qual;
 import ca.uwaterloo.watform.alloymodel.AlloyModel;
+import java.util.*;
 
 public class AlloyModelResolved extends AlloyModel {
 
-	public AlloyModelResolved(AlloyFile alloyFile)
-	{
-		super(alloyFile);
-		resolve();
-	}
-	
-	private static record SignatureRecord(AlloySigPara para, List<String> inParents, Optional<String> extendsParent, List<String> children, List<String> topLevelParents, List<String> descs, List<String> ances) {
+    public AlloyModelResolved(AlloyFile alloyFile) {
+        super(alloyFile);
+        resolve();
+    }
 
-	}
+    private static record SignatureRecord(
+            AlloySigPara para,
+            List<String> inParents,
+            Optional<String> extendsParent,
+            List<String> children,
+            List<String> topLevelParents,
+            List<String> descs,
+            List<String> ances) {}
 
-	private static record FieldRecord(String sigParent, AlloyDecl decl) {
+    private static record FieldRecord(String sigParent, AlloyDecl decl) {}
 
-	}
+    private HashMap<String, SignatureRecord> sigTable;
+    private HashMap<String, FieldRecord> fieldTable;
 
-	private HashMap<String,SignatureRecord> sigTable;
-	private HashMap<String,FieldRecord> fieldTable;
+    private void resolve() {
+        populateNames();
+    }
 
-	private void resolve()
-	{
-		populateNames();
-	}
+    private void populateNames() {
+        this.getParas(AlloySigPara.class)
+                .forEach(
+                        sp -> {
+                            String name = sp.qnames.get(0).toString();
+                            SignatureRecord r =
+                                    new SignatureRecord(sp, null, null, null, null, null, null);
+                            this.sigTable.put(name, r);
+                        });
+    }
 
-	
+    private void populateInParents() {}
 
-	private void populateNames()
-	{
-		this.getParas(AlloySigPara.class).forEach(sp -> {
-			String name = sp.qnames.get(0).toString();
-			SignatureRecord r = new SignatureRecord(sp, null, null, null, null, null, null);
-			this.sigTable.put(name, r);
-		});
-	}
+    public List<String> getAllSigNames(String signame) {
+        return new ArrayList<>(this.sigTable.keySet());
+    }
 
-	private void populateInParents()
-	{
+    public List<String> getTopLevelSigNames(String signame) {
+        return filterBy(getAllSigNames(signame), s -> this.isTopLevelSig(s));
+    }
 
-	}
+    public List<String> getDescendedSigNames(String signame) {
+        return filterBy(getAllSigNames(signame), s -> !this.isTopLevelSig(s));
+    }
 
+    public boolean isTopLevelSig(String signame) {
+        return this.sigTable.get(signame).para.isTopLevel();
+    }
 
-
-
-	public boolean isAbstractSig(String signame) {
+    public boolean isAbstractSig(String signame) {
         return this.sigTable.get(signame).para.quals.contains(Qual.ABSTRACT);
     }
 
