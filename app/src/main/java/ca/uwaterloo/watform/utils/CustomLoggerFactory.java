1ba7c86f4a3f3ebd902924dd1b9d7ce89cc113da
Mathew Kuthur James <mathewkj2048@gmail.com>
2026-02-25 12:01:31 -0500

    logger documentation

--- app/src/main/java/ca/uwaterloo/watform/utils/CustomLoggerFactory.java
+++ app/src/main/java/ca/uwaterloo/watform/utils/CustomLoggerFactory.java
@@ -5,6 +5,27 @@ import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
+/*
+
+Usage:
+
+Create a logger with a unique ID that prints logs to a specific <filename>, if debug is true.
+This creates a file called <filename>.log, which is ignored by git
+If debug is false, the logger does not print or write any of the produced logs.
+The filename can be reused with other loggers, each logger annotates the logs it writes with the part of the source code it is being used in
+
+Logger l = CustomLoggerFactory.make("<filename>",debug);
+
+l.info("test info message");
+l.warning("test warning message");
+l.fine("test fine message");
+l.finer("test finer message");
+l.config("test config message");
+
+further documentation: https://docs.oracle.com/javase/8/docs/api/java/util/logging/Logger.html
+
+*/
+
 public class CustomLoggerFactory {
 
     private static int UID = 0;
