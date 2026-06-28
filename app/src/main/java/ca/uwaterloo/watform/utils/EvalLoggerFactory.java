98443e25cf6c0a90a49bc09937b6af69de0606f8
Oleksandr Danchenko <odanchen@uwaterloo.ca>
2026-06-27 20:15:14 -0400

    added pretty logging, implemented joins and a few more set operations

---
+++ app/src/main/java/ca/uwaterloo/watform/utils/EvalLoggerFactory.java
@@ -0,0 +1,27 @@
+package ca.uwaterloo.watform.utils;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+
+public class EvalLoggerFactory {
+    private static final Map<String, EvalLogger> loggerTable = new HashMap<>();
+
+    // Class not meant to be instantiated
+    private EvalLoggerFactory() {}
+
+    // Allocates one logger per fileName
+    public static EvalLogger make(String fileName, boolean debug) {
+        if (loggerTable.containsKey(fileName)) {
+            return loggerTable.get(fileName);
+        } else {
+            var logger =
+                    new EvalLogger(
+                            CustomLoggerFactory.make(
+                                    fileName, debug, Level.ALL, new PlainFormatter()));
+            loggerTable.put(fileName, logger);
+            return logger;
+        }
+    }
+}
