738dbc8bb220d1361d333dd6e82374e034a3f38d
nancyday <nday@uwaterloo.ca>
2026-09-22 17:26:42 -0400

    missing one edit

--- app/src/main/java/ca/uwaterloo/watform/alloymodel/Qname.java
+++ app/src/main/java/ca/uwaterloo/watform/alloymodel/Qname.java
@@ -68,6 +68,10 @@ public class Qname {
 
   public static Qname fieldExprQname(AlloyQnameExpr expr) {
     assert (expr.kind == Kind.FIELD); // means it is already resolved
+    // varExpr has the qname of the form "nameSpace/sigParentName/fieldName" 
+    // as in "this/A/f"
+    // we need to tease this apart to be a Qname again
+    // as in [this, A, f]
     return new Qname(
         expr.vars.get(0).getName(), expr.vars.get(1).getName(), expr.vars.get(2).getName());
   }
