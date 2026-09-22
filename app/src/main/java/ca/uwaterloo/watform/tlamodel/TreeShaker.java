84b16b04a623bdce46d68e4a3f353621bb5ed1de
Mathew Kuthur James <mathewkj2048@gmail.com>
2026-09-22 17:06:41 -0400

    implementing recursive search to isolate unused defns

--- app/src/main/java/ca/uwaterloo/watform/tlamodel/TreeShaker.java
+++ app/src/main/java/ca/uwaterloo/watform/tlamodel/TreeShaker.java
@@ -1,5 +1,14 @@
 package ca.uwaterloo.watform.tlamodel;
 
+import static ca.uwaterloo.watform.utils.GeneralUtil.filterBy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.uwaterloo.watform.tlaast.TlaAppl;
+import ca.uwaterloo.watform.tlaast.TlaDefn;
+import ca.uwaterloo.watform.tlaexpvisitor.TlaApplCollector;
+
 public class TreeShaker {
 	public static TlaModel removeUnusedDefns(TlaModel tlaModel)
 	{
@@ -7,4 +16,36 @@ public class TreeShaker {
 
 		return answer;
 	}
+	private static List<TlaDefn> actuallyUsed(TlaModel tlaModel)
+	{
+		
+		List<TlaDefn> answer = new ArrayList<>();
+		List<TlaAppl> usedAppls = new ArrayList<>();
+		List<TlaDefn> allDefns = new ArrayList<>();
+		for(var b : tlaModel.module.body)
+			if(b instanceof TlaDefn defn)
+				allDefns.add(defn);
+		
+		// add Init and Next and constants and invariants and properties
+		var collector = new TlaApplCollector();
+		usedAppls.addAll(collector.visit(tlaModel.getInit()));
+		usedAppls.addAll(collector.visit(tlaModel.getNext()));
+		for(var prop : tlaModel.cfg.properties)
+			usedAppls.addAll(collector.visit(prop));
+		for(var inv : tlaModel.cfg.invariants)
+			usedAppls.addAll(collector.visit(inv));
+		for(var c : tlaModel.cfg.properties)
+			usedAppls.addAll(collector.visit(c));
+
+		boolean changes;
+		do
+		{
+			changes = false;
+		}
+		while(changes);
+
+		return answer;
+		
+	}
+	
 }
